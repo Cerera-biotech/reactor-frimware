8068e91adf8960bce7b00f904bb53261cdb5e61b
Andrii Romanenko <arforgethedev@gmail.com>
2021-04-26 15:12:36 +0300

    initial commit

---
+++ src/main.c
@@ -0,0 +1,23 @@
+#include "wifi_manager.h"
+
+/**
+ * @brief this is an exemple of a callback that you can setup in your own app to get notified of wifi manager event.
+ */
+void cb_connection_ok(void *pvParameter){
+	ip_event_got_ip_t* param = (ip_event_got_ip_t*)pvParameter;
+
+	/* transform IP to human readable string */
+	char str_ip[16];
+	esp_ip4addr_ntoa(&param->ip_info.ip, str_ip, IP4ADDR_STRLEN_MAX);
+
+	ESP_LOGI(TAG, "I have a connection and my IP is %s!", str_ip);
+}
+
+void app_main() {
+    /* start the wifi manager */
+	wifi_manager_start();
+
+	/* register a callback as an example to how you can integrate your code with the wifi manager */
+	wifi_manager_set_callback(WM_EVENT_STA_GOT_IP, &cb_connection_ok);
+    
+}
