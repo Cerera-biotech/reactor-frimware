ae0fce4a20835fffb2ccc6cd3d3e87e4e7a4e03a
Andrii Romanenko <arforgethedev@gmail.com>
2021-04-26 18:04:51 +0300

    http server added

--- src/main.c
+++ src/main.c
@@ -9,28 +9,49 @@
 
 #include "wifi_manager.h"
 
+#include "httpserver.h"
+
 /* @brief tag used for ESP serial console messages */
 static const char TAG[] = "main";
 
+static httpd_handle_t server = NULL;
 
 /**
  * @brief this is an exemple of a callback that you can setup in your own app to get notified of wifi manager event.
  */
-void cb_connection_ok(void *pvParameter){
-	ip_event_got_ip_t* param = (ip_event_got_ip_t*)pvParameter;
+void cb_connection_ok(void *pvParameter)
+{
+    ip_event_got_ip_t *param = (ip_event_got_ip_t *)pvParameter;
 
-	/* transform IP to human readable string */
-	char str_ip[16];
-	esp_ip4addr_ntoa(&param->ip_info.ip, str_ip, IP4ADDR_STRLEN_MAX);
+    /* transform IP to human readable string */
+    char str_ip[16];
+    esp_ip4addr_ntoa(&param->ip_info.ip, str_ip, IP4ADDR_STRLEN_MAX);
 
-	ESP_LOGI(TAG, "I have a connection and my IP is %s!", str_ip);
+    ESP_LOGI(TAG, "I have a connection and my IP is %s!", str_ip);
+
+    /* Start the server for the first time */
+    server = start_webserver();
 }
 
-void app_main() {
+/**
+ * @brief this is an exemple of a callback that you can setup in your own app to get notified of wifi manager event.
+ */
+void cb_connection_disconnected(void *pvParameter)
+{
+
+    ESP_LOGI(TAG, "lost connection!");
+
+    if (server != NULL)
+        /* Start the server for the first time */
+        stop_webserver(server);
+}
+
+void app_main()
+{
     /* start the wifi manager */
-	wifi_manager_start();
+    wifi_manager_start();
 
-	/* register a callback as an example to how you can integrate your code with the wifi manager */
-	wifi_manager_set_callback(WM_EVENT_STA_GOT_IP, &cb_connection_ok);
-
+    /* register a callback as an example to how you can integrate your code with the wifi manager */
+    wifi_manager_set_callback(WM_EVENT_STA_GOT_IP, &cb_connection_ok);
+    wifi_manager_set_callback(WM_EVENT_STA_DISCONNECTED, &cb_connection_disconnected);
 }
