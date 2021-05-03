7eed3974c0909a73fda23f407c23d6bc1d1b5bac
Andrii Romanenko <arforgethedev@gmail.com>
2021-05-03 11:19:42 +0300

    esp32 pico v3 pins used

--- include/pin_config.h
+++ include/pin_config.h
@@ -1,24 +1,21 @@
+#define GPIO_LED_CH0 20
+#define GPIO_LED_CH1 22
+#define GPIO_LED_CH2 5
+#define GPIO_LED_CH3 19
+#define GPIO_LED_CH4 26
+#define GPIO_LED_CH5 32
+#define GPIO_LED_CH6 33
+#define GPIO_LED_CH7 8
+#define GPIO_LED_CH8 7
+#define GPIO_LED_CH9 21
 
-// #define GPIO_LED_CH0 20
-// #define GPIO_LED_CH1 22
-// #define GPIO_LED_CH2 5
-// #define GPIO_LED_CH3 19
-// #define GPIO_LED_CH4 26
-// #define GPIO_LED_CH5 32
-// #define GPIO_LED_CH6 33
-// #define GPIO_LED_CH7 8
-// #define GPIO_LED_CH8 7
-// #define GPIO_LED_CH9 21
-
-#define GPIO_LED_CH0 13
-#define GPIO_LED_CH1 12
-#define GPIO_LED_CH2 27
-#define GPIO_LED_CH3 26
-#define GPIO_LED_CH4 25
-#define GPIO_LED_CH5 33
-#define GPIO_LED_CH6 32
-#define GPIO_LED_CH7 22
-#define GPIO_LED_CH8 21
-#define GPIO_LED_CH9 23
-
-#define GPIO_MODE_BUTTON 14
+// #define GPIO_LED_CH0 13
+// #define GPIO_LED_CH1 12
+// #define GPIO_LED_CH2 27
+// #define GPIO_LED_CH3 26
+// #define GPIO_LED_CH4 25
+// #define GPIO_LED_CH5 33
+// #define GPIO_LED_CH6 32
+// #define GPIO_LED_CH7 22
+// #define GPIO_LED_CH8 21
+// #define GPIO_LED_CH9 23
